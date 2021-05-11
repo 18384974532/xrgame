113d0ab9d649004b530faa96618d01fa358d52df
xuran <609085974@qq.com>
2021-05-11 10:49:35 +0800

    saves

---
+++ mota1/saves/save2/player.lua
@@ -0,0 +1,15 @@
+player = {
+	1, --level
+	0, --exp
+	1000, --life
+	0, --blue
+	100, --attack
+	100, --defence
+	1, --yellow_key
+	1, --blue_key
+	0, --red_key
+	0, --building_level
+	0 --suc
+}
+
+return player
